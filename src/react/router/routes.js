e7e3439a9713ca4a7a0ffdda0d4f4812ff95c7fa
Luiz Kim <luiz.kim@controleonline.com>
2026-02-10 14:26:00 -0300

    Change Routes

--- src/react/router/routes.js
+++ src/react/router/routes.js
@@ -1,22 +1,9 @@
-import HomePage from '@controleonline/ui-ppc/src/react/pages/displays/displayPage';
-import DefaultLayout from '@controleonline/ui-layout/src/react/layouts/DefaultLayout';
 import DisplayDetails from '@controleonline/ui-ppc/src/react/pages/displays/DisplayDetails';
 
-const withLayout = Component => props => (
-  <DefaultLayout navigation={props.navigation}>
-    <Component {...props} />
-  </DefaultLayout>
-);
+
 
 const shopRoutes = [
-  {
-    name: 'HomePage',
-    component: withLayout(HomePage),
-    options: {
-      headerShown: false,
-      title: 'Menu',
-    },
-  },
+
   {
     name: 'DisplayDetails',
     component: DisplayDetails,
