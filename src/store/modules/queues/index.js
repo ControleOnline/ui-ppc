5e9422abb4202f7d6ee710485804c4c102afc271
Luiz Kim <luiz.kim@controleonline.com>
2026-04-03 11:53:50 -0300

    Add summary state to store modules

--- src/store/modules/queues/index.js
+++ src/store/modules/queues/index.js
@@ -1,4 +1,4 @@
-import * as actions from "@controleonline/ui-default/src/store/default/actions";
+﻿import * as actions from "@controleonline/ui-default/src/store/default/actions";
 import * as getters from "@controleonline/ui-default/src/store/default/getters";
 import mutations from "@controleonline/ui-default/src/store/default/mutations";
 import * as customActions from "./customActions";
@@ -15,6 +15,7 @@ export default {
     error: "",
     violations: null,
     totalItems: 0,
+    summary: {},
     message: {},
     messages: [],
     columns: [
